e09ffab5230db91f794c0bee122bacec775d1a61
luballe <luballe@gmail.com>
2018-09-08 19:07:45 -0500

    updating...

---
+++ setupRouters.sh
@@ -0,0 +1,7 @@
+scp /home/pi/openwrt_scripts/routers/show_wifi_clients_Piso__1.sh root@192.168.1.101:/etc/config/show_wifi_clients2.sh &
+scp /home/pi/openwrt_scripts/routers/show_wifi_clients_Piso2_1.sh root@192.168.1.102:/etc/config/show_wifi_clients2.sh &
+scp /home/pi/openwrt_scripts/routers/show_wifi_clients_Piso2_2.sh root@192.168.1.103:/etc/config/show_wifi_clients2.sh &
+scp /home/pi/openwrt_scripts/routers/show_wifi_clients_Piso3_1.sh root@192.168.1.104:/etc/config/show_wifi_clients2.sh &
+scp /home/pi/openwrt_scripts/routers/show_wifi_clients_Piso3_2.sh root@192.168.1.105:/etc/config/show_wifi_clients2.sh &
+scp /home/pi/openwrt_scripts/routers/show_wifi_clients_Privado.sh root@192.168.1.106:/etc/config/show_wifi_clients2.sh &
+
